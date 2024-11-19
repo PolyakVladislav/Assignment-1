b347397b6f74fbd6d63258f8b1e16859ed65de40
Vladislav Polyak <9725860@gmail.com>
2024-11-19 18:21:11 +0200

    Fix body

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const { monitorEventLoopDelay } = require("perf_hooks");
 const app = express();
 const port = process.env.PORT
 
 
 app.use(bodyParser.json());
-app.listen(port, ()=> `The server is listening ${port}`);
+
+mongoose.connect(process.env.DB_CONNECT);
+const db = mongoose.connection; 
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", function(){
+    console.log("Connectyed to database");
+})
+
+app.listen(port, ()=> {
+    console.log(`The server is listening ${port}`)
+});
+
 
