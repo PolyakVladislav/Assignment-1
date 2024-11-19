ae9b4072aa54c1852b6e2959696454ba849900f3
dormatana101 <60754331+dormatana101@users.noreply.github.com>
2024-11-19 19:22:12 +0200

    add db and still working on connect to db

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
+require('dotenv').config(); 
+
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const { monitorEventLoopDelay } = require("perf_hooks");
 const app = express();
-const port = process.env.PORT
 
+const port = process.env.PORT ;
 
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.DB_CONNECT);
+mongoose.connect("mongodb+srv://dormatana101:Dormatana054@postcomments.berhw.mongodb.net/?retryWrites=true&w=majority&appName=PostComments");
 const db = mongoose.connection; 
 db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function(){
-    console.log("Connectyed to database");
-})
-
-app.listen(port, ()=> {
-    console.log(`The server is listening ${port}`)
+db.once("open", function() {
+    console.log("Connected to database");
 });
 
-
+app.listen(port, () => {
+    console.log(`The server is listening on port ${port}`);
+});
