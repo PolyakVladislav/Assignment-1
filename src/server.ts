b06b6f7d9fc19430449df5d6a079e0a5fedaf922
dormatana101 <60754331+dormatana101@users.noreply.github.com>
2025-01-06 09:03:48 +0200

    swagger

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,28 @@ import bodyParser from "body-parser";
 import postsRoute from "./routes/posts";
 import commentsRoute from "./routes/comments";
 import authRoutes from "./routes/auth_route";
-
+import swaggerJsDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
 const app = express();
+app.use(bodyParser.json());
+app.use("/posts", postsRoute);
+app.use("/comments", commentsRoute);
+app.use("/auth", authRoutes);
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Dor Vladi REST API",
+      version: "1.0.0",
+      description: "REST server including authentication using JWT",
+    },
+    servers: [{ url: "http://localhost:"+process.env.PORT, },],
+  },
+  apis: ["./src/routes/*.ts"],
+};
+const specs = swaggerJsDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
 
 const initApp = () => {
   return new Promise<Express>((resolve, reject) => {
